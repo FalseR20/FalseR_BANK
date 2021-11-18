2030593b19360f0f535eaeaf1da4567c89121e36
FalseR20 <kno3.2002@gmail.com>
2021-11-18 22:08:20 +0300

    db: designing, add table templates, serial <- int

--- materials/console.sql
+++ materials/console.sql
@@ -1,10 +1,10 @@
 -- #1 таблица с клиентами
 create table clients
 (
-    id       serial primary key,
-    login    char(64) not null,
-    password char(64) not null,
-    name     char(30) not null
+    id        serial primary key,
+    login     char(64) not null,
+    password  char(64) not null,
+    full_name char(64) not null
 );
 drop table clients;
 
@@ -12,10 +12,9 @@ drop table clients;
 -- #2 справочник с валютами
 create table currencies
 (
-    code   char(3) primary key, -- ISO 4217
-    name   char(30) null,
-    symbol char     null
-        check (name notnull or symbol notnull)
+    id   serial primary key,
+    code char(3) not null -- ISO 4217
+    -- country char(30) null
 );
 drop table currencies;
 
@@ -24,10 +23,10 @@ drop table currencies;
 create table courses
 (
     id          bigserial primary key,
-    currency    char(3)   not null REFERENCES currencies (code),
+    currency    int       not null references currencies (id),
     course_buy  bigint    not null, -- in BYN  надо же
     course_sale bigint    not null, -- in BYN  процентики забирать
-    change_time timestamp not null
+    change_time timestamp not null default now()
 );
 drop table courses;
 
@@ -36,8 +35,8 @@ drop table courses;
 create table accounts
 (
     id       serial primary key,
-    currency char(3)        not null references currencies (code),
-    balance  decimal(20, 2) not null
+    currency int            not null references currencies (id),
+    balance  decimal(15, 6) not null
 );
 drop table accounts;
 
@@ -45,8 +44,8 @@ drop table accounts;
 -- #5 таблица доступа клиентов к счетам (для реализации many-to-many)
 create table access
 (
-    person_id  serial not null references clients (id),
-    account_id serial not null references accounts (id)
+    person_id  int not null references clients (id),
+    account_id int not null references accounts (id)
 );
 drop table access;
 
@@ -58,23 +57,34 @@ CREATE TABLE cards
     cardholder_name char(30) not null,
     expiration_date date     not null,
     security_code   int      not null,
-    account_id      serial   not null references accounts (id),
-    cardholder_id   serial   not null references clients (id)
+    account_id      int      not null references accounts (id),
+    cardholder_id   int      not null references clients (id)
 );
 drop table cards;
 
 
--- #7 таблица с транзакциями
+-- #7 таблица с шаблонами операций
+create table templates
+(
+    id          serial primary key,
+    description char(50) not null,
+    sender      int references clients (id),
+    receiver    int references clients (id)
+);
+drop table templates;
+
+
+-- #8 таблица с транзакциями
 CREATE TABLE transactions
 (
-    id            serial primary key,
-    description   char(50)       not null,
-    time          timestamp      not null,
-    sender        serial         not null references clients (id),
-    receiver      serial         not null references clients (id),
-    currency      char(3)        not null,
-    value         decimal(20, 2) not null,
-    commission    decimal(20, 2) not null,
-    is_successful bool           not null default true
+    id         serial primary key,
+    time       timestamp      not null default now(),
+    template   int references templates (id),
+    sender     int            not null references clients (id),
+    receiver   int            not null references clients (id),
+    currency   char(3)        not null,
+    value      decimal(15, 6) not null,
+    commission decimal(15, 6) not null,
+    is_active  bool           not null default true
 );
 drop table transactions;
