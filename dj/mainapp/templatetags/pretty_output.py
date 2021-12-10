e14c0add4d4c962444faa67d1b76ae3f6ccb85f7
FalseR20 <kno3.2002@gmail.com>
2021-12-10 14:00:44 +0300

    add cut_cardholder_name

--- dj/mainapp/templatetags/pretty_output.py
+++ dj/mainapp/templatetags/pretty_output.py
@@ -9,3 +9,9 @@ register = template.Library()
 def pretty_output(number: str) -> str:
     out = "%s %s %s %s" % (number[:4], number[4:8], number[8:12], number[12:])
     return out
+
+
+@register.filter
+def cut_cardholder_name(txt: str, count: int) -> str:
+    out = txt[:count] + "..." if len(txt) > count else txt
+    return out
